691633272f960c72c20cc4169eedd77cfc8bd71a
Vincent Robert <vincent.robert@nanego.com>
2015-03-17 16:08:43 +0100

    Custom main menu

--- init.rb
+++ init.rb
@@ -18,4 +18,25 @@ Redmine::Plugin.register :redmine_events do
   menu :project_menu, :flashs, {controller: 'issues', action: 'flashs'}, :caption => :label_flashs, :param => :project_id
   menu :project_menu, :bulletins, {controller: 'bulletins', action: 'index'}, :caption => :label_bulletins, :param => :project_id
   menu :project_menu, :points, {controller: 'points', action: 'index'}, :caption => :label_points, :param => :project_id
+
+  unless Rails.env.test?
+    Redmine::MenuManager.map :top_menu do |menu|
+      menu.delete :help
+      menu.delete :administration
+      menu.delete :projects
+      menu.delete :my_page
+      menu.push :issues, { :controller => 'issues', :project_id => 'cmvoa' }, :caption => :label_issue_plural,
+                :if => Proc.new{ User.current.logged? && User.current.allowed_to?(:view_issues, nil, :global => true) }
+      menu.push :administration, { :controller => 'settings' },
+                :if => Proc.new { User.current.admin? }, :last => true
+    end
+=begin
+    Redmine::MenuManager.map :project_menu do |menu|
+      #menu.delete :settings
+      menu.delete :new_issue
+      menu.push :new_issue, { :controller => 'issues', :action => 'new' }, :param => :project_id, :caption => 'Nouvel événement',
+                :html => { :accesskey => Redmine::AccessKeys.key_for(:new_issue) }, :after => :activity
+    end
+=end
+  end
 end
