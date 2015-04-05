6dfa4b5a040d8ca7f5cbeab4189047d247cbb6a1
Vincent Robert <vincent.robert@nanego.com>
2015-04-05 23:05:54 +0200

    Init bulletins design

--- app/controllers/bulletins_controller.rb
+++ app/controllers/bulletins_controller.rb
@@ -48,54 +48,9 @@ class BulletinsController < ApplicationController
   def create
     @project = Project.find(params[:project_id]) if params[:project_id].present?
     build_new_issue_from_params
+
     related_evts = Issue.joins(:tracker).where("trackers.name LIKE '%Fiche %'")
-    @issue.description ||= ""
-
-
-    major_events = {}
-    related_evts.each do |evt|
-      domaine=evt.custom_field_value(CustomField.find_by_name('Domaines')).first
-      major_events[domaine] ||= {}
-      commune = evt.custom_field_value(CustomField.find_by_name('Commune'))
-      major_events[domaine][commune] ||= []
-      major_events[domaine][commune] << evt
-    end
-
-    @issue.description << "\n# SITUATION GENERALE - FAITS MARQUANTS"
-    major_events.each do |domaine, communes|
-      # @issue.description << "\n## #{domaine}"
-      communes.each do |commune, events|
-        events.each do |event|
-          resume = event.custom_field_value(CustomField.find_by_name('Résumé'))
-          if resume.present? && event.priority_id >= 3
-            @issue.description << "\n\n* #{event.subject} - #{resume}"
-          end
-        end
-      end
-    end
-
-    @issue.description << "\n"
-
-    events = {}
-    related_evts.each do |evt|
-      evt.custom_field_value(CustomField.find_by_name('Domaines')).each do |domaine|
-        events[domaine] ||= {}
-        commune = evt.custom_field_value(CustomField.find_by_name('Commune'))
-        events[domaine][commune] ||= []
-        events[domaine][commune] << evt
-      end
-    end
-
-    events.each do |domaine, communes|
-      @issue.description << "\n# #{domaine}"
-      communes.each do |commune, events|
-        @issue.description << "\n## #{commune}" if commune.present?
-        events.each do |event|
-          @issue.description << "\n## #{event.subject}" if event.subject.present?
-          @issue.description << "\n#{event.description}" if event.description.present?
-        end
-      end
-    end
+    generate_bulletin_description(related_evts)
 
     @issue.start_date = DateTime.parse(@issue.start_date.to_s)
     @issue.due_date = DateTime.parse(@issue.start_date.to_s)
@@ -122,6 +77,185 @@ class BulletinsController < ApplicationController
       @bulletin = @issue
       respond_to do |format|
         format.html { render :action => 'index' }
+      end
+    end
+  end
+
+  def generate_bulletin_description
+    event_description = @flash.description
+    @flash.description = <<HEADER
+      <br />
+      <div style="text-align: center;margin-top:10px;">
+        <table border="1" cellpadding="0" cellspacing="0" id="flash_header" style="border: 2px solid rgb(0, 0, 0); box-shadow: rgb(101, 101, 101) 1px 1px 1px 0px; height: 200px; margin: 10px auto auto; text-align: center; width: 98%;">
+          <tbody>
+            <tr>
+              <td><img alt="" data-rich-file-id="1" src="#{request.base_url}/system/rich/rich_files/rich_files/000/000/001/original/Logo-Re%CC%81publique-Franc%CC%A7aise.png" style="text-align: center; height: 59px; width: 100px;" /><br style="text-align: center;" />
+              <span style="text-align: center; font-family: arial, helvetica, sans-serif;"><strong>Ministère de l'Écologie, du Développement durable et de l'Énergie<br />
+              Ministère du Logement, de l'Égalité des territoires et de la Ruralité<br />
+              Service de Défense, de Sécurité et d'Intelligence Économique<br />
+              <br />
+              <span style="font-size: 18px;">FLASH CMVOA N°025</span></strong><br />
+              du #{ I18n.l Time.now, format: :complete }</span></td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+      <br />
+HEADER
+
+    @flash.description << <<RESUME
+      <div style="text-align: left;"> 
+        <table align="center" border="0" cellpadding="0" cellspacing="0" style="border: 0px solid white; width: 92%;">
+          <tbody>
+            <tr>
+              <td>
+              <div style="text-align: center;">
+              <div style="text-align: left;"><em>Sources : #{@flash.custom_field_value(CustomField.find(Setting['plugin_redmine_events']['source_field'])).join(', ')}</em></div>
+              </div>
+              <br />
+              #{@flash.custom_field_value(CustomField.find(Setting['plugin_redmine_events']['summary_field']))}
+              <br /><br />
+              <span style="font-size:12px;"><em>Cabinet MEDDE informé.</em></span></td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+      <br />
+RESUME
+
+    @flash.description << <<TITRE
+      <table border="1" cellpadding="1" cellspacing="0" id="title" style="border: 1px solid rgb(0, 0, 0); margin: auto; width: 98%; background-color: rgb(230, 230, 230);">
+        <tbody>
+          <tr>
+            <td style="text-align: center;"><span style="font-size:18px;">#{@flash.subject}</span></td>
+          </tr>
+        </tbody>
+      </table>
+      <br />
+TITRE
+
+    @flash.description << <<DOMAINES
+      <table align="center" border="1" cellpadding="0" cellspacing="0" style="width: 98%; border: 1px solid rgb(0, 0, 0);">
+        <tbody>
+          <tr>
+            <td style="text-align: center;"><strong><span style="font-size:14px;">
+              #{@flash.custom_field_value(CustomField.find(Setting['plugin_redmine_events']['domain_field'])).join('-')}
+            </span></strong></td>
+          </tr>
+        </tbody>
+      </table>
+      <br />
+DOMAINES
+
+    @flash.description << <<DESCRIPTION
+      <div style="text-align: left;">
+        <table align="center" border="0" cellpadding="0" cellspacing="0" style="width: 92%;">
+          <tbody>
+            <tr>
+              <td>#{event_description}</td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+      <br />
+DESCRIPTION
+
+  end
+
+
+  def generate_bulletin_description(related_evts)
+
+    @issue.description ||= ""
+
+    major_events = {}
+    related_evts.each do |evt|
+      domaine=evt.custom_field_value(CustomField.find_by_name('Domaines')).first
+      major_events[domaine] ||= {}
+      commune = evt.custom_field_value(CustomField.find_by_name('Commune'))
+      major_events[domaine][commune] ||= []
+      major_events[domaine][commune] << evt
+    end
+
+    @issue.description << <<HEADER
+      <br />
+      <div style="text-align: center;margin-top:10px;">
+        <table border="1" cellpadding="0" cellspacing="0" id="flash_header" style="border: 2px solid rgb(0, 0, 0); box-shadow: rgb(101, 101, 101) 1px 1px 1px 0px; height: 200px; margin: 10px auto auto; text-align: center; width: 98%;">
+          <tbody>
+            <tr>
+              <td><img alt="" data-rich-file-id="1" src="#{request.base_url}/system/rich/rich_files/rich_files/000/000/001/original/Logo-Re%CC%81publique-Franc%CC%A7aise.png" style="text-align: center; height: 59px; width: 100px;" /><br style="text-align: center;" />
+              <span style="text-align: center; font-family: arial, helvetica, sans-serif;"><strong>Ministère de l'Écologie, du Développement durable et de l'Énergie<br />
+              Ministère du Logement, de l'Égalité des territoires et de la Ruralité<br />
+              Service de Défense, de Sécurité et d'Intelligence Économique<br />
+              <br />
+              <span style="font-size: 18px;">BULLETIN  QUOTIDIEN  CMVOA N° 084</span></strong><br />
+              du #{ I18n.l @issue.start_date, format: :complete } au #{ I18n.l @issue.due_date, format: :complete }</span></td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+      <br />
+HEADER
+
+    @issue.description << <<FAITS_MARQUANTS
+      <table border="1" cellpadding="1" cellspacing="0" id="title" style="border: 1px solid rgb(0, 0, 0); margin: auto; width: 98%; background-color: rgb(220, 220, 220);">
+        <tbody>
+          <tr>
+            <td><strong><em><span style="font-size:18px;"> SITUATION GENERALE - PREVISIONS</span></em></strong></td>
+          </tr>
+        </tbody>
+      </table>
+FAITS_MARQUANTS
+
+    @issue.description << <<FAITS_MARQUANTS_RESUMES_BEGIN
+      <div style="text-align: left;"> 
+        <table align="center" border="0" cellpadding="0" cellspacing="0" style="border: 0px solid white; width: 92%;">
+          <tbody>
+            <tr>
+              <td>
+FAITS_MARQUANTS_RESUMES_BEGIN
+
+    major_events.each do |domaine, communes|
+      # @issue.description << "\n## #{domaine}"
+      communes.each do |commune, events|
+        events.each do |event|
+          resume = event.custom_field_value(CustomField.find(Setting['plugin_redmine_events']['summary_field']))
+          if resume.present? && event.priority_id >= 3
+            @issue.description << "* #{event.subject} - #{resume}"
+          end
+        end
+      end
+    end
+
+    @issue.description << <<FAITS_MARQUANTS_RESUMES_END
+            </tr>
+          </tbody>
+        </table>
+      </div>
+      <br />
+FAITS_MARQUANTS_RESUMES_END
+
+
+
+    @issue.description << "\n"
+
+    events = {}
+    related_evts.each do |evt|
+      evt.custom_field_value(CustomField.find_by_name('Domaines')).each do |domaine|
+        events[domaine] ||= {}
+        commune = evt.custom_field_value(CustomField.find_by_name('Commune'))
+        events[domaine][commune] ||= []
+        events[domaine][commune] << evt
+      end
+    end
+
+    events.each do |domaine, communes|
+      @issue.description << "\n# #{domaine}"
+      communes.each do |commune, events|
+        @issue.description << "\n## #{commune}" if commune.present?
+        events.each do |event|
+          @issue.description << "\n## #{event.subject}" if event.subject.present?
+          @issue.description << "\n#{event.description}" if event.description.present?
+        end
       end
     end
   end
