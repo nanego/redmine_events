317df7c277d93dbe1d90ba8f7ff1cb00a6ee2a83
Vincent Robert <vincent.robert@nanego.com>
2015-04-08 14:30:18 +0200

    Hide status select tag if current issue in an event

---
+++ app/overrides/issues/_attributes.rb
@@ -0,0 +1,10 @@
+Deface::Override.new :virtual_path  => "issues/_attributes",
+                     :name          => "hide-status-field",
+                     :replace       => 'p:contains("status_id")',
+                     :text          => <<-EOS
+<% if @issue.tracker.name =~ /fiche/i || @issue.tracker.blank? %>
+  <%= f.hidden_field :status_id %>
+<% else %>
+  <p><%= f.select :status_id, (@allowed_statuses.collect {|p| [p.name, p.id]}), {:required => true} %></p>
+<% end %>
+EOS
