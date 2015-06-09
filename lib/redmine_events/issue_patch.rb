8aa74daa8a6d2321b5017e97a4d8500c3577a996
Vincent Robert <vincent.robert@nanego.com>
2015-06-09 15:45:02 +0200

    include first custom field when copying issues

--- lib/redmine_events/issue_patch.rb
+++ lib/redmine_events/issue_patch.rb
@@ -6,7 +6,7 @@ class Issue < ActiveRecord::Base
   def copy_from(arg, options={})
     issue = arg.is_a?(Issue) ? arg : Issue.visible.find(arg)
     self.attributes = issue.attributes.dup.except("id", "root_id", "parent_id", "lft", "rgt", "created_on", "updated_on")
-    self.custom_field_values = issue.custom_field_values.inject({}) {|h,v| if ![4,16,17].include?(v.custom_field_id); (h[v.custom_field_id] = v.value; h) else {} end }
+    self.custom_field_values = issue.custom_field_values.inject({}) {|h,v| if ![1,4,16,17].include?(v.custom_field_id); (h[v.custom_field_id] = v.value; h) else {} end }
     self.status = issue.status
     self.author = User.current
     unless options[:attachments] == false
