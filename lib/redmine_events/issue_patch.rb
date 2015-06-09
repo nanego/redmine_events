e99fa44981a058c56c5395364e9eae275bc8e8dd
Vincent Robert <vincent.robert@nanego.com>
2015-06-09 16:34:53 +0200

    Exclude some custom fields when converting events to flashes

--- lib/redmine_events/issue_patch.rb
+++ lib/redmine_events/issue_patch.rb
@@ -6,7 +6,12 @@ class Issue < ActiveRecord::Base
   def copy_from(arg, options={})
     issue = arg.is_a?(Issue) ? arg : Issue.visible.find(arg)
     self.attributes = issue.attributes.dup.except("id", "root_id", "parent_id", "lft", "rgt", "created_on", "updated_on")
-    self.custom_field_values = issue.custom_field_values.inject({}) {|h,v| if ![1,4,16,17].include?(v.custom_field_id); (h[v.custom_field_id] = v.value; h) else {} end }
+    self.custom_field_values = issue.custom_field_values.inject({}) do |h,v|
+      unless [4,16,17].include?(v.custom_field_id) # reject some custom fields when converting to flashes / bulletins or points #TODO Make it dynamic with plugin settings
+        h[v.custom_field_id] = v.value
+      end
+      h
+    end
     self.status = issue.status
     self.author = User.current
     unless options[:attachments] == false
