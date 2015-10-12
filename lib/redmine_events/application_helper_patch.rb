54c62f547182d0a18ab4f9ae426ac7cd66a7d2b9
Vincent Robert <vincent.robert@nanego.com>
2015-10-12 14:26:09 +0200

    Do not override a complete core method but use the monkey patch only when its necessary

--- lib/redmine_events/application_helper_patch.rb
+++ lib/redmine_events/application_helper_patch.rb
@@ -2,55 +2,25 @@ require_dependency 'application_helper'
 
 module ApplicationHelper
 
-  # DO NOT render the project quick-jump box
+  # DO NOT render the project quick-jump box, except when running core tests
   def render_project_jump_box
     return
-  end
+  end unless Rails.env.test?
 
-  # Helper that formats object for html or text rendering
-  def format_object(object, html=true, &block)
-    if block_given?
-      object = yield object
-    end
-    case object.class.name
-      when 'Array'
-        object.map {|o| format_object(o, html)}.join(', ').html_safe
-      when 'Time'
-        format_time(object)
-      when 'Date'
-        format_date(object)
-      when 'Fixnum'
-        object.to_s
-      when 'Float'
-        sprintf "%.2f", object
-      when 'User'
-        html ? link_to_user(object) : object.to_s
-      when 'Project'
-        html ? link_to_project(object) : object.to_s
-      when 'Version'
-        html ? link_to_version(object) : object.to_s
-      when 'TrueClass'
-        l(:general_text_Yes)
-      when 'FalseClass'
-        l(:general_text_No)
-      when 'Issue'
-        object.visible? && html ? link_to_issue(object) : "##{object.id}"
-      when 'CustomValue', 'CustomFieldValue'
-        if object.custom_field
-          f = object.custom_field.format.formatted_custom_value(self, object, html)
-          if f.nil? || f.is_a?(String)
-            ## CUSTOM PATCH START
-            textilizable(object.value)
-            ## CUSTOM PATCH STOP
-          else
-            format_object(f, html, &block)
-          end
+  unless instance_methods.include?(:format_object_with_event)
+    def format_object_with_event(object, html=true, &block)
+      if (object.class.name == 'CustomValue' || object.class.name == 'CustomFieldValue') && object.custom_field
+        f = object.custom_field.format.formatted_custom_value(self, object, html)
+        if f.nil? || f.is_a?(String)
+          textilizable(object.value)
         else
-          object.value.to_s
+          format_object_without_event(object, html, &block)
         end
       else
-        html ? h(object) : object.to_s
+        format_object_without_event(object, html, &block)
+      end
     end
+    alias_method_chain :format_object, :event
   end
 
 end
