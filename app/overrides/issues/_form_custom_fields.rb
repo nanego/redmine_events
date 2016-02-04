b4ef536aac1cad4073f7b0f7dd96af832ccfcaa8
Vincent Robert <vincent.robert@nanego.com>
2016-02-04 16:48:56 +0100

    Add placeholder to commentable custom fields

--- app/overrides/issues/_form_custom_fields.rb
+++ app/overrides/issues/_form_custom_fields.rb
@@ -12,6 +12,6 @@ Deface::Override.new :virtual_path  => "issues/_form_custom_fields",
                      :insert_after  => 'erb[loud]:contains("custom_field_tag_with_label")',
                      :text          => <<-EOS
 <%= if value.custom_field.commentable?
-  self.text_field_tag("issue[custom_field_values]["+value.custom_field.id.to_s+"-comment]", value.comment, {:id => "issue_custom_field_comments_"+value.custom_field.id.to_s})
+  self.text_field_tag("issue[custom_field_values]["+value.custom_field.id.to_s+"-comment]", value.comment, {:id => "issue_custom_field_comments_"+value.custom_field.id.to_s, placeholder: 'Ajouter un commentaire', class: 'issue_custom_field_comments'})
 end %>
 EOS
